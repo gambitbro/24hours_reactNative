bfdd455776f8923263dddd5862522e5d08449caf
gambitbro <osofw12@gmail.com>
2024-08-13 10:50:47 +0900

    20240813

---
+++ app/(tabs)/practice/FlexDirection.tsx
@@ -0,0 +1,54 @@
+import React, {useState} from 'react';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import type { PropsWithChildren } from 'react';
+
+const FlexDirectionBasics = () => {
+    const [flexDirection, setflexDirection] = useState('column');
+};
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: 8,
+        backgroundColor: 'aliceblue',
+    },
+    box: {
+        width: 50,
+        height: 50,
+    },
+    row: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+    },
+    button: {
+        paddingHorizontal: 8,
+        paddingVertical: 6,
+        borderRadius: 4,
+        backgroundColor: 'oldlace',
+        alignSelf: 'flex-start',
+        marginHorizontal: '1%',
+        marginBottom: 6,
+        minWidth: '48%',
+        textAlign: 'center',
+    },
+    selected: {
+        backgroundColor: 'coral',
+        borderWidth: 0,
+    },
+    buttonLabel: {
+        fontSize: 12,
+        fontWeight: '500',
+        color: 'coral',
+    },
+    selectedLabel: {
+        color: 'white',
+    },
+    label: {
+        textAlign: 'center',
+        marginBottom: 10,
+        fontSize: 24,
+    },
+});
+
+export default FlexDirectionBasics;
