8d9b37c419f88ae232b198aaf40b93ddf17c7043
gambitbro <osofw12@gmail.com>
2024-08-13 14:30:24 +0900

    20240813

--- app/(tabs)/practice/FlexDirection.tsx
+++ app/(tabs)/practice/FlexDirection.tsx
@@ -4,7 +4,57 @@ import type { PropsWithChildren } from 'react';
 
 const FlexDirectionBasics = () => {
     const [flexDirection, setflexDirection] = useState('column');
+
+    return (
+        <PreviewLayout
+            label="flexDirection"
+            values={['column', 'row', 'row-reverse', 'column-reverse']}
+            selectedValue={flexDirection}
+            setSelectedValue={setflexDirection}
+        >
+            <View style={[styles.box, {backgroundColor: 'powderblue'}]} />
+            <View style={[styles.box, {backgroundColor: 'skyblue'}]} />
+            <View style={[styles.box, {backgroundColor: 'steelblue'}]} />
+        </PreviewLayout>
+    );
 };
+
+type PreviewLayoutProps = PropsWithChildren<{
+    label: string;
+    values: string[];
+    selectedValue: string;
+    setSelectedValue: (value: string) => void;
+}>;
+
+const PreviewLayout = ({
+    label,
+    children,
+    values,
+    selectedValue,
+    setSelectedValue,
+}: PreviewLayoutProps) => (
+    <View style={{padding: 10, flex: 1}}>
+        <Text style={styles.label}>{label}</Text>
+        <View style={styles.row}>
+            {values.map(value => (
+                <TouchableOpacity
+                    key={value}
+                    onPress={() => setSelectedValue(value)}
+                    style={[styles.button, selectedValue === value && styles.seleced]}
+                >
+                    <Text 
+                        style={[
+                            styles.buttonLabel,
+                            selectedValue === value && styles.selectedLabel,
+                        ]}
+                    >
+                        {value}
+                    </Text>
+                </TouchableOpacity>
+            ))}
+        </View>
+    </View>
+);
 
 
 const styles = StyleSheet.create({
